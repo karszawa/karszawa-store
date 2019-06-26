8c6a9dd8cd48f31bca8692c47d5e69c32fc468fd
Hiroaki KARASAWA <karszawa@mercari.com>
2019-06-26 12:13:18 +0900

    Install styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,34 @@
 import React from "react";
 import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+  static async getInitialProps(ctx: any) {
+    // with styled-components example
+    // https://github.com/zeit/next.js/blob/master/examples/with-styled-components/pages/_document.js
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-  render() {
-    return (
-      <html>
-        <Head>
-          <style>{`
-            body { margin: 0 }
-          `}</style>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      console.log(initialProps.styles);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            <style>{`/* reset css here */`}</style>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
