c4b4cbe37664ffb84ed232ea4a7642b39e81af60
Hiroaki KARASAWA <karszawa@mercari.com>
2019-06-27 03:19:49 +0900

    Run express server instead of next

---
+++ src/server/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import next from "next";
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get("/", (req, res) => {
+    return app.render(req, res, "/", req.query);
+  });
+
+  server.get("/items", (req, res) => {
+    return app.render(req, res, "/Items", req.query);
+  });
+
+  server.get("/items/:id", (req, res) => {
+    return app.render(req, res, "/ItemsId", { id: req.params.id });
+  });
+
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
